ab7a52c9978fa70b8a9bf584abff149c8452d1d4
U-ck-laptop\ck <ck@ck-laptop.(none)>
2013-07-13 02:18:38 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject mango "0.1.0"
+  :description ""
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.novemberain/monger "1.4.2"]
+                 [com.novemberain/validateur "1.2.0"]
+                 [cheshire "5.0.1"]
+                 [clj-time "0.4.4"]])
---
+++ src/mango/core.clj
@@ -0,0 +1,145 @@
+(ns mango.core
+  (:refer-clojure :exclude [count find] :rename {first core-first})
+  (:use [monger.conversion :only [to-object-id]])
+  (:require [monger.collection :as monger]
+            [validateur.validation :as validateur]
+            [monger db core joda-time]))
+
+;Connection
+(def connect!
+  "Alias for monger.core/connect!"
+  monger.core/connect!)
+
+(def db!
+  "Composes monger.core/set-db! to monger.core/get-db"
+  (comp monger.core/set-db! monger.core/get-db))
+
+(def connect-with-db!
+  "Calls connect! and db! then returns a map containing :conn and :db keys."
+  (comp (partial hash-map :conn (connect!) :db) db!))
+
+(def disconnect!
+  "Alias for monger.core/disconnect!"
+  monger.core/disconnect!)
+
+;DB
+(def get-database-names
+  "Alias for monger.core/get-db-names"
+  monger.core/get-db-names)
+
+(def get-collection-names
+  "Alias for monger.db/get-collection-names"
+  monger.db/get-collection-names)
+
+(def drop-db
+  "Alias for monger.db/drop-db"
+  monger.db/drop-db)
+
+;Validateurs
+(defn wrap
+  "Allows to create easily a new validateur.
+  Argument must be a function."
+  [f]
+  #(fn [attribute] (apply f attribute %&)))
+
+(def ^:dynamic *validateurs*
+  "All the validateurs functions.
+  You may rebind *validateurs* easily with with-validateurs."
+  {:length (wrap #(apply validateur/length-of %1 :is %&))
+   :length-within (wrap #(apply validateur/length-of %1 :within (range %2 (inc %3)) %&))
+   :accepted (wrap validateur/acceptance-of)
+   :excluded (wrap #(apply validateur/exclusion-of %1 :in %&))
+   :included (wrap #(apply validateur/inclusion-of %1 :in %&))
+   :formated (wrap #(apply validateur/format-of %1 :format %&))
+   :numericality (wrap validateur/numericality-of)
+   :required (wrap validateur/presence-of)
+   :email (wrap #(apply validateur/format-of %1
+     :format #"(?i)^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$" %&))
+   :url (wrap #(apply validateur/format-of %1
+     :format #"(?ix)^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$" %&))})
+
+(defmacro with-validateurs
+  "Rebings easily the *validateur* variable.
+  (with-validateurs {:foo validateur-function}
+    (valid? collection document))"
+  [validateurs & body]
+  `(binding [*validateurs* (merge *validateurs* ~validateurs)]
+     ~@body))
+
+;Collection
+(defprotocol Searchable
+  (all [this])
+  (find [this conds])
+  (find-one [this conds])
+  (find-by-id [this id])
+  (first [this])
+  ;as all records implement by default clojure.lang.IPersistentMap and its (count [this]) function
+  ;count is named count-all here
+  (count-all [this])
+  (count [this conds]))
+
+(defprotocol Writable
+  (save [this document])
+  (insert [this document])
+  (update [this conds document])
+  (update-by-id [this id document]))
+
+(defprotocol Deletable
+  (delete-all [this])
+  (delete [this conds])
+  (delete-by-id [this id]))
+
+(defprotocol Validable
+  (valid [this document])
+  (valid? [this document]))
+
+(defrecord Collection [name attributes validations]
+  Searchable
+  (all [this] (find this {}))
+  (find [this conds] (monger/find-maps name conds))
+  (find-one [this conds] (monger/find-one-as-map name conds))
+  (find-by-id [this id] (monger/find-map-by-id name id))
+  (first [this] (find-one this {}))
+  (count-all [this] (monger/count name))
+  (count [this conds] (monger/count name conds))
+
+  Writable
+  ;add :created_at and :updated_at keys
+  (save [this document]
+    (if (get document :_id)
+      (monger/save-and-return name document)
+      (insert this document)))
+  (insert [this document] (monger/insert-and-return name document))
+  (update [this conds document] (monger/update name conds document))
+  (update-by-id [this id document] (monger/update-by-id name (to-object-id id) document))
+
+  Deletable
+  (delete-all [this] (monger/remove name))
+  (delete [this conds] (monger/remove name conds))
+  (delete-by-id [this id] (monger/remove-by-id name (to-object-id id)))
+
+  Validable
+  (valid [this document] ((apply validateur/validation-set validations) document))
+  (valid? [this document] (empty? (valid this document))))
+
+(defn- collection
+  "Returns a hash map with :attributes and :validations"
+  [constraints]
+  (hash-map
+    :attributes (keys constraints)
+    :validations (for [[attr validations] constraints
+                       validation validations]
+                   (condp apply [validation]
+                     vector? ((apply ((core-first validation) *validateurs*) (rest validation)) attr)
+                     keyword? (((validation *validateurs*)) attr)))))
+
+(defmacro defcollection
+  "Defines a new Collection.
+  (defcollection users
+    (username [:length-within 6 255])
+    (password [:length-within 6 255])
+    (email :email [:length-within 6 255]))"
+  [name & constraints]
+  (let [{:keys [attributes validations]}
+        (collection (reduce (fn [agg [attr & vs]] (conj agg [(keyword attr) vs])) {} constraints))]
+    `(def ~name (Collection. ~(str name) '~attributes '~validations))))
