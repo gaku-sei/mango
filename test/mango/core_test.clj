2ee85ddba09e8a38db5641f52c0b77ddc7ba2a44
U-ck-laptop\ck <ck@ck-laptop.(none)>
2013-07-13 17:01:33 +0200

    First tests written

--- test/mango/core_test.clj
+++ test/mango/core_test.clj
@@ -1,7 +1,25 @@
 (ns mango.core-test
+  (:refer-clojure :exclude [count find first])
   (:require [clojure.test :refer :all]
             [mango.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(connect-with-db! "mango")
+
+(defcollection users
+  (username :required [:length-within 6 255])
+  (password :required [:length-within 6 255])
+  (email :required :email [:length-within 6 255])
+  (website [:url :allow-nil true]))
+
+(def valid-user
+  {:username "foobar" :password "foobar" :email "foo@bar.baz" :website "http://www.foobar.baz"})
+
+(deftest collection
+  (testing "defcollection and validations"
+    (are [x y] (= x y)
+         (:name users) "users"
+         (sort (:attributes users)) (sort [:username :password :email :website]))
+    (is (valid? users valid-user))))
+
+(drop-db)
+(disconnect!)
